443afad7bed6f8831e33938778c54d74dbbe6584
luxiaol <lu_xiaol@126.com>
2017-10-13 16:33:49 +0800

    init pod project
    Signed-off-by: luxiaol <lu_xiaol@126.com>

---
+++ MyPrivatePods/Classes/MyTestObject.h
@@ -0,0 +1,12 @@
+//
+//  MyTestObject.h
+//  MyPrivatePods
+//
+//  Created by lu_xiaol on 2017/10/13.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyTestObject : NSObject
+
+@end
---
+++ MyPrivatePods/Classes/MyTestObject.m
@@ -0,0 +1,12 @@
+//
+//  MyTestObject.m
+//  MyPrivatePods
+//
+//  Created by lu_xiaol on 2017/10/13.
+//
+
+#import "MyTestObject.h"
+
+@implementation MyTestObject
+
+@end
